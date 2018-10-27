980fd7b37f64e20228a5aad86e66c62df79960c8
CunYe <784396136@qq.com>
2018-10-27 09:20:54 +0800

    添加中间件

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,20 @@ Route::get('/login',"LoginController@login")->name('login');
 // 注册
 Route::get('/register',"LoginController@register")->name('register');
 
+// 添加登录中间件
+Route::middleware(['login'])->group(function(){
+    // 主页
+    Route::get('/',"IndexController@index")->name('index');
 
-// 主页
-Route::get('/',"IndexController@index")->name('index');
+    // 首页
+    Route::get('/home',"IndexController@home")->name('home');
 
-// 首页
-Route::get('/home',"IndexController@home")->name('home');
+    // 基本管理
+    Route::get('/basic/seller',"BasicController@seller")->name('basicSeller'); // 修改资料
+    Route::get('/basic/pwd',"BasicController@pwd")->name('basicPwd'); // 修改密码
 
-// 基本管理
-Route::get('/basic/seller',"BasicController@seller")->name('basicSeller'); // 修改资料
-Route::get('/basic/pwd',"BasicController@pwd")->name('basicPwd'); // 修改密码
+    // 商品管理
+    Route::get('/goods/add',"GoodsController@add")->name('goodsAdd'); // 新增商品
+    Route::get('/goods/goods',"GoodsController@goods")->name('goods'); // 商品管理
+});
 
-// 商品管理
-Route::get('/goods/add',"GoodsController@add")->name('goodsAdd'); // 新增商品
-Route::get('/goods/goods',"GoodsController@goods")->name('goods'); // 商品管理
